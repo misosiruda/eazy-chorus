b0f9d04aa7a1c64f825e2e689dbd86140fa4c18a
misosiruda <81164760+misosiruda@users.noreply.github.com>
2026-05-07 11:06:53 +0900

    add part mark editor domain

---
+++ src/features/part-editor/index.ts
@@ -0,0 +1,7 @@
+export {
+  getPartMarksForSegment,
+  splitSegmentTextByPartMarks,
+  togglePartMark,
+  updateCueSegmentRole,
+} from './partEditor'
+export type { PartMarkTextFragment } from './partEditor'
